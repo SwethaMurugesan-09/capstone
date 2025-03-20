182947f4a2e8431158296c52633235343e630870
Swetha Murugesan <swetham.22cse@kongu.edu>
2025-03-20 14:02:57 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@
     docker login -u swethamurugesan -p dckr_pat_j4nie2mMiPKS3B6DXcDP-GSg7-o
     docker tag test1 swethamurugesan/devopsgit
     docker push swethamurugesan/devopsgit
-    
+    docker-compose up -d    
